9358e34a83bf0256040baf9dfc04231e963e5b72
Luyanda Mdanda <lmdanda@ucsd.edu>
2018-06-05 12:58:41 -0700

    Restructuring processing to retain subject information

--- scripts/analysis_eeg_dat.py
+++ scripts/analysis_eeg_dat.py
@@ -10,157 +10,104 @@ from fooof.analysis import *
 from pathlib import Path
 
 ####################################################################################################
-####################################################################################################
+## SETTINGS ##
 
+DATASET = 'rtPB'
+STATE = 'rest'
+
+####################################################################################################
+####################################################################################################
 
 # Set up paths
 # This results and analysis path will need updating
-rest_results_path = 'D:\\abc\\Documents\\Research\\Results\\Rest_Results'
-trial_results_path = 'D:\\abc\\Documents\\Research\\Results\\Trial_Results'
+# Set data path based on STATE
+if STATE == 'rest':
+	results_path = 'D:\\abc\\Documents\\Research\\Results\\Rest_Results'
+elif STATE == 'trial':
+	results_path = 'D:\\abc\\Documents\\Research\\Results\\Trial_Results'
 
-	# These should stay the same
-subj_dat_num1 =list(range(3001, 3015))
-subj_dat_num2 = list(range(3502, 3516))
-all_subj = subj_dat_num1 + subj_dat_num2
+# DATASET properties
+# Properties of rtPB Dataset
+if DATASET == 'rtPB':
+	subj_dat_num = list(range(3502, 3516))
+	if STATE == 'rest':
+		num_blocks = 2
+	elif STATE == 'trial':
+		num_blocks = 10 
+	n_subjects = len(subj_dat_num)
+# Properties of PBA Dataset
+elif DATASET == 'PBA':
+	subj_dat_num = list(range(3001, 3015))
+	if STATE == 'rest':
+		num_blocks = 1
+	elif STATE == 'trial':
+		num_blocks = 12 
+	n_subjects = len(subj_dat_num)
+
+# General properties
+#number of channels
+n_channels = 64
+
+# Set up indexes for accessing data, for convenience
+cf_ind, am_ind, bw_ind = 0, 1, 2
+n_feats = 3
+
+# Define bands of interest
+theta_band = [2, 7]
+alpha_band = [7, 14]
+beta_band = [15, 30]
 
 def main():
 
-
-
-		# GET LIST OF FILES
-	rest_f_names = fnmatch.filter(os.listdir(rest_results_path), '*fooof_group_results?.json')
-	trial_f_names = fnmatch.filter(os.listdir(trial_results_path), '*fooof_group_results?.json')
-	rest_n_subjects = len(rest_f_names)
-	trial_n_subjects = len(trial_f_names)
-
-	#number of channels
-	n_channels = 64
-
-	# Set up indexes for accessing data, for convenience
-	cf_ind, am_ind, bw_ind = 0, 1, 2
-
-	# Define bands of interest
-	theta_band = [2, 7]
-	alpha_band = [7, 14]
-	beta_band = [15, 30]
+	# GET LIST OF FILES - OUTDATED 
+	#rest_f_names = fnmatch.filter(os.listdir(rest_results_path), '*fooof_group_results?.json')
+	#trial_f_names = fnmatch.filter(os.listdir(trial_results_path), '*fooof_group_results?.json')
 
 	# Initialize 3D group arrays
-	rest_group_cube_results_theta = np.empty(shape=[n_channels, 3, rest_n_subjects])
-	rest_group_cube_results_alpha = np.empty(shape=[n_channels, 3, rest_n_subjects])
-	rest_group_cube_results_beta = np.empty(shape=[n_channels, 3, rest_n_subjects])
-
-
-	trial_group_cube_results_theta = np.empty(shape=[n_channels, 3, trial_n_subjects])
-	trial_group_cube_results_alpha = np.empty(shape=[n_channels, 3, trial_n_subjects])
-	trial_group_cube_results_beta = np.empty(shape=[n_channels, 3, trial_n_subjects])
-
-
+	thetas = np.zeros(shape=[n_subjects, num_blocks, n_channels, n_feats])
+	alphas = np.zeros(shape=[n_subjects, num_blocks, n_channels, n_feats])
+	betas = np.zeros(shape=[n_subjects, num_blocks, n_channels, n_feats])
 
 	# START LOOP
-	#for sub in subj_dat_num:
-	for subj_ind, subj_f_name in enumerate(rest_f_names):
-		subj_file = os.path.splitext(subj_f_name)[0]
-		print('Current Subject Results: ' + subj_file)
-
-		# load results data
-		fg = FOOOFGroup()
-		full_path = os.path.join(rest_results_path, subj_f_name)
-		path_check = Path(full_path)
-		if path_check.is_file():
-			fg.load(file_name=subj_file, file_path=rest_results_path)
-
+	# DATASET: PBA 
+	for sub_index, sub_num in enumerate(subj_dat_num):
+		print('Current Subject Results: ' + str(sub_num))
+		
+		# load rest results data
+		for block in range(0, num_blocks):
+			subj_file = str(sub_num) + "fooof_group_results" + str(block) + ".json"
+			fg = FOOOFGroup()
+			full_path = os.path.join(results_path, subj_file)
+			path_check = Path(full_path)
+			if path_check.is_file():
+				fg.load(file_name=subj_file, file_path=results_path)
 			if not fg.group_results:
-				print('Current Subject Results: ' + subj_f_name + " failed to load")
+				print('Current Subject Results: ' + str(sub_num) + " failed to load")
 			else:
-				print('Current Subject Results: ' + subj_f_name + " successfully loaded")
+				print('Current Subject Results: ' +  str(sub_num) +" successfully loaded")
 
 
-			# Initializae the 3d array
-			#ind_cube_results = np.empty(n_channels, 3, 4)
 
-			# Initialize arrays to store band oscillations - 2D arrays per band, per subj
-			thetas = np.empty(shape=[n_channels, 3])
-			alphas = np.empty(shape=[n_channels, 3])
-			betas = np.empty(shape=[n_channels, 3])
-
-
-			# Extract all alpha oscillations from FOOOFGroup results
-			#  Note that this preserves information about which PSD each oscillation comes from
 			for ind, res in enumerate(fg):
-				# TODO: adding these into the matrices that are initialized above
-				thetas[ind, :] = get_band_peak(res.peak_params, theta_band, True)
-				alphas[ind, :] = get_band_peak(res.peak_params, alpha_band, True)
-				betas[ind, :] = get_band_peak(res.peak_params, beta_band, True)
+				thetas[ind, block, :,  :] = get_band_peak(res.peak_params, theta_band, True)
+				alphas[ind, block, :,  :] = get_band_peak(res.peak_params, alpha_band, True)
+				betas[ind, block, :,  :] = get_band_peak(res.peak_params, beta_band, True)
 
-
-			# COLLECT INTO GROUP
-			rest_group_cube_results_theta[:, :, subj_ind] = thetas
-			rest_group_cube_results_alpha[:, :, subj_ind] = alphas
-			rest_group_cube_results_beta[:, :, subj_ind] = betas
-
-
+			#alphas[sub_ind, block, :, :] = get_band_peak_group()
+			#betas[sub_ind, block, :, :] = get_band_peak_group()
 
 			# Save out matrices
-			np.save('..\\data\\rest_theta_group.npy', rest_group_cube_results_theta)
-			np.save('..\\data\\rest_alpha_group.npy', rest_group_cube_results_alpha)
-			np.save('..\\data\\rest_beta_group.npy', rest_group_cube_results_beta)
+			# Update to save out files using DATASET and STATE
+			np.save('..\\data\\' + DATASET + "_" + STATE + "_thetas.npy" , thetas)
+			np.save('..\\data\\' + DATASET + "_" + STATE + "_alphas.npy", alphas)
+			np.save('..\\data\\' + DATASET + "_" + STATE + "_betas.npy", betas)
 
 			print("File SAVED")
 		else:
-			print('Current Subject' + str(sub)+ ' does not exist')
+			print('Current Subject' + str(sub_num)+ ' does not exist')
 
-	for subj_ind, subj_f_name in enumerate(trial_f_names):
-		subj_file = os.path.splitext(subj_f_name)[0]
-		print('Current Subject Results: ' + subj_file)
-
-		# load results data
-		fg = FOOOFGroup()
-		full_path = os.path.join(trial_results_path, subj_f_name)
-		path_check = Path(full_path)
-		if path_check.is_file():
-			fg.load(file_name=subj_file, file_path=trial_results_path)
-
-			if not fg.group_results:
-				print('Current Subject Results: ' + subj_f_name + " failed to load")
-			else:
-				print('Current Subject Results: ' + subj_f_name + " successfully loaded")
-
-			# Initializae the 3d array
-			#ind_cube_results = np.empty(n_channels, 3, 4)
-
-			# Initialize arrays to store band oscillations - 2D arrays per band, per subj
-			thetas = np.empty(shape=[n_channels, 3])
-			alphas = np.empty(shape=[n_channels, 3])
-			betas = np.empty(shape=[n_channels, 3])
-
-
-			# Extract all alpha oscillations from FOOOFGroup results
-			#  Note that this preserves information about which PSD each oscillation comes from
-			for ind, res in enumerate(fg):
-				# TODO: adding these into the matrices that are initialized above
-				thetas[ind, :] = get_band_peak(res.peak_params, theta_band, True)
-				alphas[ind, :] = get_band_peak(res.peak_params, alpha_band, True)
-				betas[ind, :] = get_band_peak(res.peak_params, beta_band, True)
-
-
-			# COLLECT INTO GROUP
-			trial_group_cube_results_theta[:, :, subj_ind] = thetas
-			trial_group_cube_results_alpha[:, :, subj_ind] = alphas
-			trial_group_cube_results_beta[:, :, subj_ind] = betas
-
-
-
-			# Save out matrices
-			np.save('..\\data\\trial_theta_group.npy', trial_group_cube_results_theta)
-			np.save('..\\data\\trial_alpha_group.npy', trial_group_cube_results_alpha)
-			np.save('..\\data\\trial_beta_group.npy', trial_group_cube_results_beta)
-
-			print("File SAVED")
-		else:
-			print('Current Subject' + str(sub)+ ' does not exist')
 
 if __name__ == "__main__":
 	main()
-
 
 
