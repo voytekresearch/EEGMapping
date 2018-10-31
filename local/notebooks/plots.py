d26f2bfbf92e863f7daa53423d0a63d64a836e0f
TomDonoghue <tdonoghue@ucsd.edu>
2018-10-31 14:54:17 -0700

    ReOrg stuff

--- local/notebooks/plots.py
+++ local/notebooks/plots.py
@@ -1,110 +1,62 @@
 """Plotting functions for EEG-Mapping analysis."""
+
 import os
+
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import ttest_1samp, ttest_ind, sem, pearsonr
-from sklearn.preprocessing import scale
-from utilities import *
-import mne
 from matplotlib import cm
 
+from scipy.stats import ttest_1samp, ttest_ind, sem, pearsonr
+
+import mne
+
+from utilities import *
 
 ###################################################################################################
 ###################################################################################################
 
 def save_figure(save_out, name):
+
     fig_save_path = 'C:\\Users\\abc\\Documents\\Research\\figures'
     if save_out:
         plt.savefig(os.path.join(fig_save_path, name + '.png'))
 
 
 def plot_comp(title, feature, dat1, dat2, save_fig=False, save_name=None):
-    """Plot comparison between groups, as a mean value with an errorbar.
-    Dat should be 1D vector, with data that can be split up by YOUNG & OLD _INDS.
-    """
+    """Plot comparison between groups, as a mean value with an errorbar."""
+
     fig, ax = plt.subplots(figsize=[2, 4])
+
     # Split up data
     dat1 = dat1[~np.isnan(dat1)]
     dat2 = dat2[~np.isnan(dat2)]
 
     means = [np.mean(dat1), np.mean(dat2)]
-
     sems = [sem(dat1), sem(dat2)]
+
     plt.errorbar([1, 2], means, yerr=sems, xerr=None, fmt='.',
                  markersize=22, capsize=10, elinewidth=2, capthick=2)
-    
+
     ax.set_xlim([0.5, 2.5])
     plt.xticks([1, 2], ['Trial', 'Rest'])
-    
+
     # Titles & Labels
     ax.set_title(title)
     ax.set_xlabel('State')
     ax.set_ylabel(feature)
 
-    save_figure(save_fig, title +"_" + feature + "_across_state")
+    save_figure(save_fig, title + "_" + feature + "_across_state")
 
 
-def make_across_blocks(curr_data, dataset, pos_ch_cluster_index, SAVE_FIGS):
+def plot_across_blocks(means, stds, name):
 
-    bands = ["alpha", "beta", "theta"]
-    feats = ["CFS", "AMPS", "BWS"]
-
-    for band in bands:
-        for feat_in, feat in enumerate(feats):
-            
-            for s_ind in range(dataset[band].shape[0]):
-                for ch_ind in range(dataset[band].shape[2]):
-                    for f_ind in range(dataset[band].shape[3]):
-                        dataset[band][s_ind, :, ch_ind, f_ind] = scale(dataset[band][s_ind, :, ch_ind, f_ind], with_mean=True, axis=0, with_std=False)
-                        
-            demeaned_curr_masked_data = np.take(dataset[band], indices=pos_ch_cluster_index,  axis=2 )
-            demeaned_curr_mean_data = np.nanmean(demeaned_curr_masked_data, axis = 2)
-            demeaned_curr_data_matrix = demeaned_curr_mean_data[:,:,feat_in]
-            
-            #plt.figure()
-            means = np.nanmean(demeaned_curr_data_matrix, axis = 0)
-            stds = np.std(demeaned_curr_data_matrix, axis = 0)
-            plt.figure()
-            plt.errorbar(range(0, demeaned_curr_data_matrix.shape[1]), means, yerr=stds, xerr=None, fmt='.',
-                 		 markersize=22, capsize=10, elinewidth=2, capthick=2)
-            #plt.plot(demeaned_curr_data_matrix.T, ".");
-            save_figure(SAVE_FIGS, curr_data + "_" + band + "_" + feat + "_across_blocks_plot")
-
-
-
-def make_slope_across_blocks(curr_data, dataset, pos_ch_cluster_index, SAVE_FIGS):
-
-    feats = ["Offset", "Slope"]
-    
-    for feat_in, feat in enumerate(feats):
-
-        for s_ind in range(dataset.shape[0]):
-            for ch_ind in range(dataset.shape[2]):
-                for f_ind in range(dataset.shape[3]):
-                    dataset[s_ind, :, ch_ind, f_ind] = scale(dataset[s_ind, :, ch_ind, f_ind],
-                                                             with_mean=True, axis=0, with_std=False)
-
-        demeaned_curr_masked_data = np.take(dataset, indices=pos_ch_cluster_index,  axis=2)
-        demeaned_curr_mean_data = np.nanmean(demeaned_curr_masked_data, axis=2)
-        demeaned_curr_data_matrix = demeaned_curr_mean_data[:,:,feat_in]
-        
-        
-        avg_across_subjs = np.nanmedian(demeaned_curr_data_matrix, axis=0)
-        print(avg_across_subjs.shape)
-        
-        corr = pearsonr(avg_across_subjs, range(0,dataset.shape[1]))
-        print(str(corr) + " for the dataset: " + curr_data + "_" + feat)
-
-        means = np.nanmean(demeaned_curr_data_matrix, axis = 0)
-        stds = np.std(demeaned_curr_data_matrix, axis = 0)
         plt.figure()
-        plt.errorbar(range(0, demeaned_curr_data_matrix.shape[1]), means, yerr=stds, xerr=None, fmt='.',
-                 		 markersize=22, capsize=10, elinewidth=2, capthick=2)
-        save_figure(SAVE_FIGS, curr_data + "_" + feat + "_across_blocks_plot")
+        plt.errorbar(range(len(means)), means, yerr=stds, xerr=None, fmt='.',
+                     markersize=22, capsize=10, elinewidth=2, capthick=2)
+        save_figure(SAVE_FIGS, name)
 
 
-
-def make_topos(datasets, state, eeg_dat_info, pos, SAVE_FIGS = True):
+def make_topos(datasets, state, eeg_dat_info, pos, SAVE_FIGS=True):
     """
     datasets: list of 4d arrays (?)
     """
@@ -114,7 +66,6 @@ def make_topos(datasets, state, eeg_dat_info, pos, SAVE_FIGS = True):
 
     for band in bands:
         for feat_in, feat in enumerate(feats):
-            #print(feat_in)
 
             topo_dat = np.zeros(shape=[2, 64])
 
@@ -124,13 +75,13 @@ def make_topos(datasets, state, eeg_dat_info, pos, SAVE_FIGS = True):
             plot_topo(topo_dat[0, :], title='D1' + state + band + feat, eeg_dat_info=eeg_dat_info)
             plot_topo(topo_dat[0, :], title='D2' + state + band + feat, eeg_dat_info=eeg_dat_info)
             plot_topo(np.mean(topo_dat, 0), title='Both' + state + band + feat, eeg_dat_info=eeg_dat_info)
-            
+
             #medial to lateral
             plt.figure()
             plt.scatter(abs(pos[:, 0]), np.mean(topo_dat,0))
             pearsonr(abs(pos[:, 0]), np.mean(topo_dat,0))
             save_figure(SAVE_FIGS, 'Both_' + state + band +  feat + "_medial_to_lateral_plot")
-        
+
             # posterior to anterior
             plt.figure()
             plt.scatter(pos[:, 1], np.mean(topo_dat,0))
@@ -138,21 +89,19 @@ def make_topos(datasets, state, eeg_dat_info, pos, SAVE_FIGS = True):
             save_figure(SAVE_FIGS, 'Both_' + state + band + feat + "_posterior_to_anterior_plot")
 
 
-
 def plot_topo(data, title, eeg_dat_info):
     """
     data: 1d array, len number of channels
     title: string
     """
-    
+
     inds = np.where(np.isnan(data))
     data[inds] = np.nanmean(data)
-    
+
     fig, ax = plt.subplots()
     mne.viz.plot_topomap(data, eeg_dat_info, cmap=cm.viridis, contours=0, axes=ax)
     fig_save_path = 'C:\\Users\\abc\\Documents\\Research\\figures'
     fig.savefig(os.path.join(fig_save_path, title + '.png'), dpi=600)
-
 
 
 def make_slope_topos(datasets,state, eeg_dat_info, pos, SAVE_FIGS = True):
@@ -165,6 +114,7 @@ def make_slope_topos(datasets,state, eeg_dat_info, pos, SAVE_FIGS = True):
 
         for ind, dataset in enumerate(datasets):
             topo_dat[ind, :] =  avg_for_slope_topo(dataset, feat_in)
+
         print (np.mean(topo_dat, axis = 0))
         plot_topo(topo_dat[0, :], title='D1' + state + feat, eeg_dat_info=eeg_dat_info)
 
@@ -177,7 +127,7 @@ def make_slope_topos(datasets,state, eeg_dat_info, pos, SAVE_FIGS = True):
         plt.scatter(abs(pos[:, 0]), np.mean(topo_dat,0))
         pearsonr(abs(pos[:, 0]), np.mean(topo_dat,0))
         save_figure(SAVE_FIGS, 'Both_' + state + feat + "_medial_to_anterior_plot")
-        
+
         # posterior to anterior
         plt.figure()
         plt.scatter(pos[:, 1], np.mean(topo_dat,0))
--- local/notebooks/utilities.py
+++ local/notebooks/utilities.py
@@ -1,6 +1,9 @@
 """Utilities functions for EEG-Mapping analysis."""
 
+import warnings
+
 import numpy as np
+from sklearn.preprocessing import scale
 
 ###################################################################################################
 ###################################################################################################
@@ -9,7 +12,7 @@ def mask_nan_array(dat):
     return ~np.isnan(dat)
 
 
-def combine_groups(dataset1, dataset2):    
+def combine_groups(dataset1, dataset2):
 	bands = ["alpha", "beta", "theta"]
 	output = dict()
 	for band in bands:
@@ -28,11 +31,11 @@ def avg_for_topo(dataset, band, feat_in):
     dataset: 4d array
     band: string
     feat_in: int
-    """ 
+    """
     feat_set =  dataset[band][:,:,:,feat_in]
     bloc_aver_set = np.nanmean(feat_set, axis = 1)
     bloc_aver_set = np.nanmean(bloc_aver_set, axis = 0)
-    
+
     return bloc_aver_set
 
 
@@ -45,16 +48,76 @@ def avg_for_slope_topo(dataset, feat_in):
     feat_set =  dataset[:,:,:,feat_in]
     bloc_aver_set = np.nanmean(feat_set, axis = 1)
     bloc_aver_set = np.nanmean(bloc_aver_set, axis = 0)
-    
+
     return bloc_aver_set
 
 
 def masking_cluster(pos_ch_cluster, eeg_dat):
-    """Returns the index positions of electrodes to be masked after acception the name of these electrodes as input.
+    """Returns the index positions of electrodes to be masked after
+        accepting the name of these electrodes as input.
     """
+
     pos_ch_cluster_index = []
     for pos_ch in pos_ch_cluster:
         pos_ch_cluster_index.append(eeg_dat.info['ch_names'].index(pos_ch))
 
     # Check the indices for the channel cluster
     return pos_ch_cluster_index
+
+
+def demean(dataset):
+    """Demean data.
+
+    dataset: 4d array, [n_subjs, n_blocks, n_chs, n_feats]
+    """
+
+    for s_ind in range(dataset.shape[0]):
+        for ch_ind in range(dataset.shape[2]):
+            for f_ind in range(dataset.shape[3]):
+
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore")
+                    dataset[s_ind, :, ch_ind, f_ind] = scale(dataset[s_ind, :, ch_ind, f_ind],
+                                                             with_mean=True, axis=0, with_std=False)
+
+    return dataset
+
+
+def run_dict_across_blocks(label, dataset, ch_indices, SAVE_FIGS):
+    """
+    label: str
+    dataset: dict
+    ch_indices: list of str
+    SAVE_FIGS: bool
+    """
+
+    bands = dataset.keys()
+    feat_labels = ["CFS", "AMPS", "BWS"]
+
+    for band in bands:
+
+        curr_data = dataset[band]
+        run_array_across_blocks(label, dataset, ch_indices, feat_labels=feat_labels, SAVE_FIGS)
+
+
+def run_array_across_blocks(label, dataset, ch_indices, feat_labels, SAVE_FIGS):
+    """Run analysis of FOOOF features across blocks.
+    label:
+    dataset:
+    ch_indices
+    feat_labels:
+    SAVE_FIGS:
+    """
+
+    for feat_in, feat in enumerate(feat_labels):
+
+        dataset = demean(dataset)
+
+        demeaned_curr_masked_data = np.take(dataset, indices=ch_indices,  axis=2)
+        demeaned_curr_mean_data = np.nanmean(demeaned_curr_masked_data, axis=2)
+        demeaned_curr_data_matrix = demeaned_curr_mean_data[:,:,feat_in]
+
+        means = np.nanmean(demeaned_curr_data_matrix, axis=0)
+        stds = np.std(demeaned_curr_data_matrix, axis=0)
+
+        plot_across_blocks(means, stds, label + "_" + band + "_" + feat + "_across_blocks_plot")
